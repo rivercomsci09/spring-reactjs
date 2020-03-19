01feb01e73ec14333a3a0900130f354918f4f854
Tran Truong Giang <rivercomsci09@gmail.com>
2020-03-19 21:33:14 +0700

    create spring project

---
+++ devicesmanagement/src/main/java/com/river/devicesmanagement/DevicesmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.river.devicesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevicesmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevicesmanagementApplication.class, args);
+    }
+
+}
