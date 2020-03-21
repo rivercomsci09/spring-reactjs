ca058fe6b253a5cb0e21cbeb802b3df6627077f6
Tran Truong Giang <rivercomsci09@gmail.com>
2020-03-21 22:00:40 +0700

    Resolve conflic

--- devicesmanagement/src/main/java/com/river/devicesmanagement/controller/ApplicationController.java
+++ devicesmanagement/src/main/java/com/river/devicesmanagement/controller/ApplicationController.java
@@ -4,24 +4,36 @@ import com.river.devicesmanagement.model.Account;
 import com.river.devicesmanagement.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ApplicationController {
     @Autowired
     AccountService accountService;
 
-    @RequestMapping(value = "/accounts", method = RequestMethod.GET)
+    @RequestMapping(value = "/accounts", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Account>> findAllAccount() {
         List<Account> accounts = accountService.findAllAccount();
         if (accounts.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(accounts, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/accounts/{id}",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Account> getAccountById(@PathVariable("id") Integer id) {
+        Optional<Account> account = accountService.findById(id);
+        if (!account.isPresent()) {
+            return new ResponseEntity<>(account.get(),HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(account.get(), HttpStatus.OK);
     }
 }
