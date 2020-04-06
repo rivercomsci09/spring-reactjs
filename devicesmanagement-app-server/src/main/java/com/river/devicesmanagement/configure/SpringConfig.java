e45ffa60d97ed77ad6673ccdf32a293c0898397e
Tran Truong Giang <rivercomsci09@gmail.com>
2020-04-06 16:04:29 +0700

    Modified SpringConfig

--- devicesmanagement-app-server/src/main/java/com/river/devicesmanagement/configure/SpringConfig.java
+++ devicesmanagement-app-server/src/main/java/com/river/devicesmanagement/configure/SpringConfig.java
@@ -3,14 +3,18 @@ package com.river.devicesmanagement.configure;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class SpringConfig implements WebMvcConfigurer {
+    private final long MAX_AGE_SECS = 3600;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5000");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
     }
 }
